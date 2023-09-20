9ebe89faf2da7ac802bec5023adac70ddf140523
kennankole <kennankole@gmail.com>
2023-09-20 13:18:32 +0300

    Adding index.css in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,9 +66,10 @@ import UpdateBlogPost from './features/blog/BlogUpdate';
 import BlogList from './features/blog/BlogList';
 import BlogPage from './features/blog/Blog';
 
-import './assets/styles/Kenya.css';
-import './assets/styles/Blog.css';
-import './assets/styles/HomePage.css';
+import "./index.css";
+// import './assets/styles/Kenya.css';
+// import './assets/styles/Blog.css';
+// import './assets/styles/HomePage.css';
 
 const App = () => {
 
